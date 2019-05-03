b9feb816c65c24a27b4166616a1457a47f51a958
Austin Bingham <austin.bingham@gmail.com>
2019-05-03 09:22:53 +0200

    Erg.

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,19 @@
 use std::iter::FromIterator;
 use std::iter::Iterator;
 
-fn sort(xs: &mut Vec<i32>) {
-
+pub fn sort(xs: &mut Vec<i32>) {
     for index in 1..xs.len() {
         let current = xs[index];
 
-        let back_index = xs[0..index].iter().enumerate().rev()
-            .find_map(|(bi, &x)| if current >= x { Some(bi) } else { None} )
-            .unwrap_or(0);
+        let back_index = xs[0..index]
+            .iter()
+            .enumerate()
+            .find_map(|(idx, &x)| if current < x { Some(idx) } else { None })
+            .unwrap_or(index);
 
         // Can we remove this copy?
         let to_shift = Vec::from_iter(xs[back_index..index].iter().cloned());
-        xs.splice(back_index+1..index+1, to_shift);
+        xs.splice(back_index + 1..index + 1, to_shift);
         xs[back_index] = current;
     }
 }
@@ -23,8 +24,22 @@ mod tests {
 
     #[test]
     fn basic_sort() {
-        let mut xs = vec![3,2,1];
+        let mut xs = vec![3, 2, 1];
         sort(&mut xs);
-        assert_eq!(xs, vec![1,2,3]);
+        assert_eq!(xs, vec![1, 2, 3]);
+    }
+
+    #[test]
+    fn sort_empty() {
+        let mut xs = vec![];
+        sort(&mut xs);
+        assert_eq!(xs, vec![]);
+    }
+
+    #[test]
+    fn sort_sorted() {
+        let mut xs = vec![1, 2, 3];
+        sort(&mut xs);
+        assert_eq!(xs, vec![1, 2, 3]);
     }
 }
