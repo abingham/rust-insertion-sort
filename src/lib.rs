b14de91ba044c1ab464904941482ab6122cd46ab
Austin Bingham <austin.bingham@gmail.com>
2019-05-03 09:59:59 +0200

    Generic implementation.

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,20 @@
 use std::iter::FromIterator;
 use std::iter::Iterator;
 
-pub fn sort(xs: &mut Vec<i32>) {
+pub fn sort<I: PartialOrd + Copy>(xs: &mut Vec<I>) {
     for current_index in 1..xs.len() {
-        let current = xs[current_index];
+        let current = &xs[current_index];
 
         match xs[0..current_index]
             .iter()
             .enumerate()
-            .find_map(|(idx, &x)| if current < x { Some(idx) } else { None })
+            .find_map(|(idx, &x)| if current < &x { Some(idx) } else { None })
         {
             Some(insertion_index) => {
                 let to_shift = Vec::from_iter(xs[insertion_index..current_index].iter().cloned());
+                let current = current.clone();
                 xs.splice(insertion_index + 1..current_index + 1, to_shift);
-                xs[insertion_index] = current;
+                xs[insertion_index] = current.clone();
             }
             _ => {}
         }
@@ -33,7 +34,7 @@ mod tests {
 
     #[test]
     fn sort_empty() {
-        let mut xs = vec![];
+        let mut xs: Vec<i32> = vec![];
         sort(&mut xs);
         assert_eq!(xs, vec![]);
     }
